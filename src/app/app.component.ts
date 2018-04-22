00fdb0cf23ced7c9ccab0e3cb5d44eee500d3adb
mukajihh <mukajihh@gmail.com>
2018-04-22 18:30:31 -0300

    centralize components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './taskList/taskList.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './taskList/taskList.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
